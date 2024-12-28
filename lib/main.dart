2f7d48e9510475bcf92d3ef6c390af9e085cb396
Bartłomiej Płóciennik <bplociennik03@gmail.com>
2024-12-28 13:03:59 +0100

    adding firebase config to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:trials_of_valhalla/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
