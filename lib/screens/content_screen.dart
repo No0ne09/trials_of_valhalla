aad793a4c37e40092538221a7a59ecef5ac935b5
Bartłomiej Płóciennik <bplociennik03@gmail.com>
2024-12-28 17:52:11 +0100

    fixed bug with orientation on ios

--- lib/screens/content_screen.dart
+++ lib/screens/content_screen.dart
@@ -1,10 +1,44 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ContentScreen extends StatelessWidget {
+class ContentScreen extends StatefulWidget {
   const ContentScreen({super.key});
 
   @override
+  State<ContentScreen> createState() => _ContentScreenState();
+}
+
+class _ContentScreenState extends State<ContentScreen> {
+  @override
+  void initState() {
+    super.initState();
+    SystemChrome.setPreferredOrientations([]);
+    SystemChrome.setPreferredOrientations(
+        [DeviceOrientation.landscapeRight, DeviceOrientation.landscapeLeft]);
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Icon(Icons.play_arrow),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Icon(Icons.settings),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Icon(Icons.leaderboard),
+          ),
+        ],
+      ),
+    );
   }
 }
