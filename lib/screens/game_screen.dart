924efcc388b46adf67109c2a2822704c73832283
Bartłomiej Płóciennik <bplociennik03@gmail.com>
2024-12-29 14:34:38 +0100

    starting with game added parallax

--- lib/screens/game_screen.dart
+++ lib/screens/game_screen.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/game.dart';
+import 'package:flame/parallax.dart';
 import 'package:flutter/material.dart';
 
 class GameScreen extends StatelessWidget {
@@ -5,6 +10,26 @@ class GameScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GameWidget(game: Game());
+  }
+}
+
+class Game extends FlameGame with HasCollisionDetection {
+  @override
+  FutureOr<void> onLoad() async {
+    final parallaxBackground = await loadParallaxComponent(
+      [
+        ParallaxImageData('parallax/1.png'),
+        ParallaxImageData('parallax/2.png'),
+        ParallaxImageData('parallax/3.png'),
+        ParallaxImageData('parallax/4.png'),
+        ParallaxImageData('parallax/5.png'),
+        ParallaxImageData('parallax/6.png'),
+      ],
+      baseVelocity: Vector2(10, 0),
+      velocityMultiplierDelta: Vector2(1.4, 0),
+    );
+    add(parallaxBackground);
+    return super.onLoad();
   }
 }
